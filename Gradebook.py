d6cb45848bc8b88e539f21fc7079c29c5817d42b
Ana Brunner <anabrunner.ab@gmail.com>
2023-03-06 22:37:50 -0800

    Added a histogram plotting function and more.

--- Gradebook.py
+++ Gradebook.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 #Some test data while building the functions. Each student has an unique student ID in case their name is not unique.
 student_info = {
     1 : {
@@ -36,6 +39,23 @@ def get_average(numbers):
         total += num
     return float(total) / len(numbers)
 
+#Calculates the median of a list of numbers
+def get_median(numbers):
+    numbers.sort()
+    length_half = int(len(numbers) / 2)
+    if len(numbers) % 2 != 0:
+        return numbers[length_half]
+    else:
+        return ((numbers[length_half] + numbers[length_half - 1]) / 2)
+
+#Calculates the standard deviation of a list of numbers
+def get_stdev(numbers):
+    average = get_average(numbers)
+    sum_difference = 0
+    for num in numbers:
+        sum_difference += (num - average) ** 2
+    return (sum_difference / len(numbers)) ** 0.5
+
 #Calculates a student's grade average
 def student_average(student_id):
     grades = []
@@ -63,3 +83,19 @@ def class_average(subject):
         if subject in student_info[i]["Grade"].keys():
             grades.append(student_info[i]["Grade"][subject])
     return get_average(grades)
+
+#Plots a histogram of the class grades
+def plot_grade_histogram(subject):
+    grades = []
+    for i in student_info:
+        if subject in student_info[i]["Grade"].keys():
+            grades.append(student_info[i]["Grade"][subject])
+    plt.hist(grades, 10, (0,100))
+    plt.xlabel("Grade")
+    plt.ylabel("Number of students")
+    plt.title("%s Grades" % str(subject))
+    plt.figtext(0.01, 0.01, r'Avg. = %s, St. dev. = %s' % (str(round(get_average(grades), 1)), str(round(get_stdev(grades), 1))))
+    plt.axis([0, 100, 0, 0])
+    plt.autoscale(True, 'y')
+    plt.grid(color = 'grey', linestyle = '--', linewidth = 0.5)
+    return plt.show()
